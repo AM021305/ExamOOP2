039132648adf8834df81b621cef98945a70c8c52
AM021952 <xyz4240@gmail.com>
2015-04-28 17:39:13 +0800

    A AM021952 龍盛琳提交 txtCopy

--- Question2.java
+++ Question2.java
@@ -1,7 +1,20 @@
 ﻿public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
-		editFile("Dest.txt");
-		updateFile("Dest.txt");
+		//editFile("Dest.txt");
+		//updateFile("Dest.txt");
+	}
+
+
+	public static void  txtCopy(String a,String b) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new FileReader("Source.txt"));
+		
+		String line = null;
+		String allLine = "";
+		while ((line = br.readLine()) != null){
+			System.out.println(line);
+		}
+		br.close();
 	}
 }
